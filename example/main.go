51d69f9a9a4658b323296e9ffafde5659a80e942
nozomi0966 <nozomotoitech@gmail.com>
2019-06-23 10:43:26 +0900

    remove type assation

--- example/main.go
+++ example/main.go
@@ -5,12 +5,9 @@ import (
 	"time"
 
 	taskqueue "github.com/nozo-moto/taskqueue"
-	"github.com/pkg/profile"
 )
 
 func main() {
-	defer profile.Start(profile.CPUProfile, profile.ProfilePath(".")).Stop()
-
 	taskQueue := taskqueue.New(100 * time.Millisecond)
 	go taskQueue.Run()
 	fmt.Println("task run ")
@@ -21,33 +18,22 @@ func main() {
 	for {
 		index := i + 1
 		err := taskQueue.Add(
-			func(args ...interface{}) error {
-				index, ok := args[0].([]interface{})[0].(int)
-				if !ok {
-					panic("error assation not good")
-				}
+			func() error {
 				fmt.Printf("hoge %d\n", index)
-
 				return nil
 			},
 			3,
-			index,
 		)
 		if err != nil {
 			fmt.Println("error", err)
 			break
 		}
 		err = taskQueue.Add(
-			func(args ...interface{}) error {
-				index, ok := args[0].([]interface{})[0].(int)
-				if !ok {
-					panic("hoge")
-				}
+			func() error {
 				fmt.Printf("error %d\n", index)
 				return fmt.Errorf("hoge %d", index)
 			},
 			2,
-			index,
 		)
 		if err != nil {
 			fmt.Println("error", err)
